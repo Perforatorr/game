f625b59ab2533206af05b2df295699cf70230ab7
Egor <donskoje723@gmail.com>
2026-08-06 20:48:55 +0300

    teeeeeest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from Vorota import roots_game
 from Vorota import roots_scene
 from start import left_path
-from start import wind_game
 from game_test import right_path
-from game_test import whisper_game
 from game_test import clear
 
 
